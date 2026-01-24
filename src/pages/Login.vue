24a099c88359c9c9c02989f796dfea425e7778d3
hohuy <hhuy85895@gmail.com>
2026-01-24 11:34:47 +0700

    luu

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -29,15 +29,22 @@
               />
             </div>
 
+          
             <button type="submit" class="btn btn-login">Đăng Nhập</button>
           
             <div class="divider">
               <span>Hoặc</span>
             </div>
 
-            <router-link to="/phanhoidangky">
+            <router-link to="/phanhoidangky" style="margin-bottom: 20px;">
               <button type="button" class="btn btn-register">Đăng ký</button>
             </router-link>
+
+            <router-link to="/quenmatkhau" style="margin-left: 60%; color: blue; text-decoration: none;">
+              Quên mật khẩu
+            </router-link>
+
+
 
           </form>
         </div>
